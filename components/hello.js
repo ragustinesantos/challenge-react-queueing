68e068bf8e28d6026cc96a8a87d1ae5a22ad258b
ragustinesantos <112927560+ragustinesantos@users.noreply.github.com>
2023-04-29 13:14:02 +0800

    Initial commit
    Initial file upload to main

---
+++ components/hello.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function lodi() {
+    return (
+        <div>'Hello'</div>
+    )
+}
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import lodi from "../components/hello";
+
+function App() {
+
+  return (
+    <lodi />
+  )
+}
+
+export default App;
